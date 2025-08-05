9989bf8e0343a892795c7e4025820f4fa1ae3e6b
calebandria <kalebaandriamanaja@gmail.com>
2025-08-05 14:28:51 +0300

    fix: modifying app.json to include supabase keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,13 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gtqpgewedhbprmebghhr.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0cXBnZXdlZGhicHJtZWJnaGhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4ODc3ODIsImV4cCI6MjA1ODQ2Mzc4Mn0.VXGqqNep5bU5BDuQE1NCrm2xQYz0I1uBiEipUBY0ODg';
+const supabaseUrl: string | undefined = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey: string | undefined = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase environment variables are missing!');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
